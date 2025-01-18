d3494eff8dbbd4f9858b99d15739ac0b351c66c9
explodeTheComputer <dnfjddk2@gmail.com>
2025-01-18 10:47:08 +0900

    asdf

--- Main.java
+++ Main.java
@@ -1,37 +1,17 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    static class Point {
-        int x, y;
 
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();// one input
+        // String strings[] = br.readLine().split(" ");//two input
+        // StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = Integer.parseInt(scan.nextLine());
-        ArrayList<Point> points = new ArrayList<>(n);
+        // System.out.println(sb);
 
-        for (int i = 0; i < n; i++) {
-            String[] token = scan.nextLine().split(" ");
-            points.add(new Point(Integer.parseInt(token[0]), Integer.parseInt(token[1])));
-        }
-
-        Collections.sort(points, (p1, p2) -> {
-            if (p1.x == p2.x)
-                return p1.y - p2.y;
-            return p1.x - p2.x;
-        });
-
-        for (Point p : points) {
-            System.out.println(p.x + " " + p.y);
-        }
-
-        scan.close();
+        br.close();
     }
 }
